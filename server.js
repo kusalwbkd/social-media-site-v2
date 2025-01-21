51548355271715bf08f0a4e5d458a5eea2f544c4
kuslwbkd <kusalwbkddhananjaya@gmail.com>
2025-01-21 19:44:59 +0530

    some changes

--- server.js
+++ server.js
@@ -34,11 +34,11 @@ cloudinary.config({
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 
-app.use(express.static(path.resolve(__dirname, './frontend/dist')));
+app.use(express.static(path.resolve(__dirname, './public')));
 
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, './frontend/dist', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './public', 'index.html'));
 });
 
 
